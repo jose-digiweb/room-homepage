edd1f1cd03629f4a6be9e3e6bc94bf2de25bc137
josefurtado <josefurtado.digital@gmail.com>
2021-04-24 14:24:25 +0200

    Hamburger Functional

--- app/stylejs/main.js
+++ app/stylejs/main.js
@@ -1,1 +1,24 @@
-console.log('Hello World!');
+const mobileOpen = document.querySelector('.menu__mobile');
+const mobileClose = document.querySelector('.mobile__close');
+const headerNav = document.querySelector('.header');
+const menuOverlay = document.querySelector('.overlay__menu');
+
+const handlingOpen = (e) => {
+  e.preventDefault();
+
+  headerNav.style.transform = 'translateY(0)';
+  menuOverlay.style.visibility = 'visible';
+  menuOverlay.style.opacity = '0.6';
+};
+
+const handlingClose = (e) => {
+  e.preventDefault();
+
+  headerNav.style.transform = 'translateY(-113px)';
+  menuOverlay.style.opacity = '0';
+  menuOverlay.style.visibility = 'hidden';
+};
+
+mobileOpen.addEventListener('click', handlingOpen);
+
+mobileClose.addEventListener('click', handlingClose);
